28d643d8e3e154120046d840b03de57d38d30376
Francesco Pantano <fpantano@redhat.com>
2021-02-17 13:01:35 +0100

    CephPlacementSpec is able to produce the placement spec
    A few improvements are provided by this change:
    
    1. multiple cli examples
    2. label list is processed in the main area
    3. A Placement pattern and priority based on the host list
       is provided
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- scripts/mkspec.py
+++ scripts/mkspec.py
@@ -20,33 +20,93 @@ import yaml
 import json
 import sys
 
-# NOTES:
-#
-#
+# NOTES/TODO(s):
+# 1. add count keyword  (lower priority since it's fixed in OpenStack)
+# 2. hosts should be turned into a list before being processed by the proper class
+# 3. we should be alble (in main) to specify an array of CephHostSpec
 
 ALLOWED_DAEMONS = ['host', 'mon', 'mgr', 'mds', 'nfs', 'osd', 'rgw', 'grafana', \
                    'prometheus', 'alertmanager', 'node-exporter']
 
-ALLOWED_HOST_PATTERN = ['list', 'regex']
+ALLOWED_HOST_PLACEMENT_MODE = ['hosts', 'host_pattern', 'label']
+
+class CephPlacementSpec(object):
+    def __init__(self,
+            hosts: list,
+            host_pattern: str,
+            count: int,
+            labels: list):
+
+        #if len(labels) > 0:
+        #    self.labels = labels
+        #if count > 0:
+        #    self.count = count
+        #if host_pattern is not None and len(host_pattern) > 0:
+        #    self.host_pattern = host_pattern
+
+        self.hosts = hosts
+        self.host_pattern = host_pattern
+        self.count = count
+        self.labels = labels
+
+    def __setattr__(self, key, value):
+        self.__dict__[key] = value
+
+    def make_spec(self):
+        # if the host list is passed, this should be
+        # the preferred way
+        if len(self.hosts) > 0:
+            spec_template = {
+                'placement': {
+                    'hosts': self.hosts
+                }
+            }
+        # if no list is passed or an empty list is provided
+        # let's check if a "host pattern" is provided
+        elif getattr(self, 'host_pattern', None):
+            spec_template = {
+                'placement': {
+                    'host_pattern': self.host_pattern
+                }
+            }
+        elif getattr(self, 'labels', None) is not None:
+            spec_template = {
+                'placement': {
+                    'labels': self.labels
+                }
+            }
+        else:
+            spec_template = {}
+
+        # TODO: Add count to the list of placement parameters
+        return spec_template
 
 class CephHostSpec(object):
     def __init__(self, daemon_type: str,
                  daemon_addr: str,
                  daemon_hostname: str,
                  labels: list[str]):
+
         self.daemon_type = daemon_type
         self.daemon_addr = daemon_addr
         self.daemon_hostname = daemon_hostname
+
         assert isinstance(labels, list)
         self.labels = labels
 
     def make_daemon_spec(self):
+        lb = {}
+
         spec_template = {
             'service_type': self.daemon_type,
             'addr': self.daemon_addr,
             'hostname': self.daemon_hostname,
-            'labels': self.labels
         }
+
+        if len(self.labels) > 0:
+            lb = {'labels': self.labels}
+
+        spec_template = {**spec_template, **lb}
         return (yaml.dump(spec_template, indent=2))
 
 class CephDaemonSpec(object):
@@ -54,47 +114,39 @@ class CephDaemonSpec(object):
                  daemon_id: str,
                  daemon_name: str,
                  placement: str,
-                 spec: dict):
+                 spec: dict,
+                 labels: list):
 
         self.daemon_name = daemon_name
         self.daemon_id = daemon_id
         self.daemon_type = daemon_type
         self.placement = placement
+        self.labels = labels
 
         assert isinstance(spec, dict)
         self.spec = spec
 
-    def host_list(self) -> list:
-        '''
-        we're assuming here a comma separated list
-        because those values are provided via cli
-        '''
-
-        hl = []
-        for e in self.placement.split(','):
-            hl.append(e)
-        return hl
-
     def make_daemon_spec(self):
+
+        pl = {}
+        sp = {}
+
+        placement = CephPlacementSpec([], "", 0, self.labels)
+
+        pl = placement.make_spec()
+
         spec_template = {
-            # improve how the placement can be defined
-            # 1. a dict where a pattern can be specified
-            # 2. the host list that can be passed
-            'placement': {
-                'hosts': self.host_list()
-            },
             'service_type': self.daemon_type,
             'service_name': self.daemon_name,
-            'service_id': self.daemon_id
+            'service_id': self.daemon_id,
         }
 
-        sp = {}
         if len(self.spec.keys()) > 0:
             sp = {'spec': self.spec}
 
         # build the resulting daemon template
-        s = {**spec_template, **sp}
-        return (yaml.dump(s, indent=2))
+        spec_template = {**spec_template, **pl, **sp}
+        return (yaml.dump(spec_template, indent=2))
 
     def log(self, msg):
         print('[DEBUG] - %s' % msg)
@@ -108,7 +160,8 @@ def export(content, preview):
         print(content)
     if len(content) > 0 and OPTS.output_file:
         fname = OPTS.output_file
-        with open(fname, 'w') as f:
+        with open(fname, 'a') as f:
+            f.write('---\n')
             f.write(content)
 
 
@@ -135,7 +188,7 @@ def parse_opts(argv):
                         help=("The address of the host we're going to apply"))
     parser.add_argument('-z', '--hostname', metavar='hostname',
                         help=("The hostname of the host we're going to apply"))
-    parser.add_argument('-l', '--label', metavar='label',
+    parser.add_argument('-l', '--labels', metavar='labels',
                         help=("The labels of the host we're going to apply"),
                         default=[])
     parser.add_argument('-o', '--output-file', metavar='OUT_FILE',
@@ -146,11 +199,26 @@ def parse_opts(argv):
 
     return opts
 
+# -- CLI utilities -- #
+
+def str_to_list(iterator) -> list:
+    '''
+    we're assuming here a comma separated list
+    because those values are provided via cli
+    '''
+
+    hl = []
+    for e in iterator.split(','):
+        hl.append(e)
+    return hl
+
 
 if __name__ == "__main__":
 
     OPTS = parse_opts(sys.argv)
     spec = {}
+    labels = []
+    hosts = []
 
     if OPTS.daemon not in ALLOWED_DAEMONS:
         print('Error, unable to render the spec for an Unknown Ceph daemon!')
@@ -162,19 +230,38 @@ if __name__ == "__main__":
     if OPTS.service_name is None:
         OPTS.service_name = OPTS.daemon
 
+    if len(OPTS.labels) > 0:
+        labels = str_to_list(OPTS.labels)
+
     if len(OPTS.spec) > 0:
         spec = json.loads(OPTS.spec.replace("'", "\""))
 
     if OPTS.daemon == "host":
-        d = CephHostSpec(OPTS.daemon, OPTS.address, OPTS.hostname, ['mon', 'mgr'])
+        d = CephHostSpec(OPTS.daemon, OPTS.address, OPTS.hostname, labels)
     else:
-        d = CephDaemonSpec(OPTS.daemon, OPTS.service_id, OPTS.service_name, OPTS.placement, spec)
+        d = CephDaemonSpec(OPTS.daemon, \
+                OPTS.service_id, \
+                OPTS.service_name, \
+                OPTS.placement, \
+                spec, \
+                labels)
 
     # Export the host I built in the specified output file
     export(d.make_daemon_spec(), True)
 
 
-# e.g.
+# ------------ EXAMPLE OF CREATING A NEW HOST ---------------- #
+
+# mkspec.py -d host -a standalone -z standalone
+#addr: standalone
+#hostname: standalone
+#labels:
+#- mon
+#- mgr
+#service_type: host
+
+# ------------ EXAMPLE OF DAEMON USING A HOST LIST ---------------- #
+
 # mkspec.py -d rgw -p host1,host2,host3 -s "{'zone' : 'default'}" -o rgw_out
 #placement:
 #  hosts:
@@ -186,11 +273,16 @@ if __name__ == "__main__":
 #service_type: rgw
 #spec:
 #  zone: default
-# ----
-# mkspec.py -d host -a standalone -z standalone
-#addr: standalone
-#hostname: standalone
-#labels:
-#- mon
-#- mgr
-#service_type: host
+
+# ------------ EXAMPLE WITH LABELS ---------------- #
+
+#> python mkspec.py -d rgw -p host1,host2,host3 -s "{'zone' : 'default'}" -l mon,rgw -o rgw_out
+#placement:
+#  labels:
+#  - mon
+#  - rgw
+#service_id: rgw
+#service_name: rgw
+#service_type: rgw
+#spec:
+#  zone: default
