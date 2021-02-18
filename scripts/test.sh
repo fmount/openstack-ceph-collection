601e745c2f04466c312b5fe6d7e897309a2fb580
Francesco Pantano <fpantano@redhat.com>
2021-02-18 13:38:29 +0100

    Host list and host_pattern are now separated options
    The placement class is built providing (as attributes)
    the way the placement section should be rendered in the
    spec.
    This commit adds two different parameters to make sure
    we can properly discern when a host_pattern is provided
    instead of a host list.
    Tests are also refactored a bit (but it's under heavy
    development).
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -25,25 +25,38 @@ ceph_cluster['osd3']='osd_hostname_3'
 
 
 # Building hosts
-for key in "${!ceph_cluster[@]}"; do
-    host="$key"
-    hostname=${ceph_cluster["$key"]}
-    addr=${hostlist["${ceph_cluster["$key"]}"]}
-    case "$host" in
-      *mon*) label="mon" ;;
-      *osd*) label="osd" ;;
-    esac
-    python mkspec.py -d 'host' -a $hostname -z $addr -l $label
-done
+test_host_list() {
+  for key in "${!ceph_cluster[@]}"; do
+      host="$key"
+      hostname=${ceph_cluster["$key"]}
+      addr=${hostlist["${ceph_cluster["$key"]}"]}
+      case "$host" in
+        *mon*) label="mon" ;;
+        *osd*) label="osd" ;;
+      esac
+      python mkspec.py -d 'host' -a $hostname -z $addr -l $label
+  done
 
-# mons - Add the minimal amount of daemons
-python mkspec.py -d mon -p "${ceph_cluster['mon1']}","${ceph_cluster['mon2']}","${ceph_cluster['mon3']}"
+  # mons - Add the minimal amount of daemons
+  python mkspec.py -d mon -g "${ceph_cluster['mon1']}","${ceph_cluster['mon2']}","${ceph_cluster['mon3']}"
 
-# osds - Add the minimal amount of daemons
-python mkspec.py -d osd -i default_drive_group -n osd.default_drive_group \
-    -p ${ceph_cluster['osd1']},${ceph_cluster['osd2']},${ceph_cluster['osd3']} \
-    -s "{'data_devices':{'paths': [ '/dev/ceph_vg/ceph_lv_data'] }}"
+  # osds - Add the minimal amount of daemons
+  python mkspec.py -d osd -i default_drive_group -n osd.default_drive_group \
+      -g ${ceph_cluster['osd1']},${ceph_cluster['osd2']},${ceph_cluster['osd3']} \
+      -s "{'data_devices':{'paths': [ '/dev/ceph_vg/ceph_lv_data'] }}"
 
-# crash - Add the crash daemon everywhere
+  # crash - Add the crash daemon everywhere
+  python mkspec.py -d crash -p '*aaa*'
+}
 
-python mkspec.py -d crash -p '*'
+test_host_pattern() {
+
+  # mds - Add the mds daemon on controllers
+  python mkspec.py -d mds -p "*controller*" -o test_host_pattern
+
+  # node-exporter - Add this service everywhere in the cluster
+  python mkspec.py -d node-exporter -p "*" -o test_host_pattern
+
+}
+
+test_host_list
