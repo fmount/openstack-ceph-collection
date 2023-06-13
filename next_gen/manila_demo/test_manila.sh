adfff30a7d3a3157618352c2890e34705775e7c9
Goutham Pacha Ravi <gouthampravi@gmail.com>
2023-06-13 00:06:12 -0700

    Add more manila tests for the demo

--- next_gen/manila_demo/test_manila.sh
+++ next_gen/manila_demo/test_manila.sh
@@ -9,6 +9,7 @@ function test_manila {
     declare -a share1=("/etc/ceph")
     declare -a share2=("/etc/ceph")
     declare -a share3= "/etc/manila")
+    alias openstack="oc rsh openstackclient openstack"
 
     echo "---------------------"
     echo "MANILA PREVIEW"
@@ -49,6 +50,96 @@ function test_manila {
         oc describe secret $sec
         echo "----------------------"
     fi
+
+    sleep $TIME
+    manila_services=$(openstack share service list)
+    if [ -n "$manila_services" ]; then
+        echo "OpenStack Manila Services ($ openstack share service list):"
+        echo $manila_services
+    fi
+
+    sleep $TIME
+    pools=$(openstack share pool list)
+    if [ -n "$pools" ]; then
+        echo "The storage pools known to OpenStack Manila "
+        echo "($ openstack share pool list):"
+        echo $pools
+    fi
+
+    # Creating share types
+    sleep $TIME
+    if [ -n "$pools" ]; then
+        echo "----------------------"
+        echo "Let's create a default share type that will direct provisioning to Ceph"
+        openstack share type create default False \
+                  --extra-specs snapshot_support=True  \
+                  create_share_from_snapshot_support=True vendor_name=Ceph
+        echo "Let's create a share type that we'll use with NetApp"
+        openstack share type create bigboxtype False \
+                  --extra-specs snapshot_support=True  \
+                  revert_to_snapshot_support=True
+                  create_share_from_snapshot_support=True
+    fi
+
+    # Creating and displaying shares and their export paths
+    sleep $TIME
+    if [ -n "$pools" ]; then
+        echo "----------------------"
+        echo "Let's create an Native CephFS share: "
+        openstack share create cephfs 10 --name cephfs-share
+        echo "Let's create an NFS share: "
+        openstack share create nfs 10 --name nfs-share
+        echo "And a CIFS share (will end up on NetApp)"
+        openstack share create cifs 10 --name cifs-share --share-type bigboxtype
+        echo "----------------------"
+        sleep $TIME
+        echo "Export locations of these shares"
+        openstack share export location list cephfs-share
+        openstack share export location list nfs-share
+        openstack share export location list cifs-share
+
+        nfs_export=$(openstack share export location list nfs-share -c Path -f value)
+        cephfs_export=$(openstack share export location list cephfs-share -c Path -f value)
+    fi
+
+    # allowing access and mounting shares
+    # we have two clients, baremetal nodes with IP addresses 10.10.10.40 and
+    # 10.10.10.50; we'll allow access and mount the NFS share in 10.10.10.40
+    # and the CEPHFS share in 10.10.10.50
+    openstack share access create nfs-share ip 10.10.10.40
+    openstack share access create cephfs-share cephx alice
+
+    sleep $TIME
+    openstack share access list nfs-share
+    openstack share access list cephfs-share
+
+    cephx_secret=$(openstack share access list cephfs-share -c "Access Secret" -f value)
+
+    # mounting the shares
+    echo "Mounting the NFS share on client1 with IP address 10.10.10.40"
+    ssh root@10.10.10.40 mount -t nfs $nfs_export /mnt
+    ssh root@10.10.10.40 touch /mnt/test1 /mnt/test2 /mnt/test3
+
+    echo "Testing client restrictions, 10.10.10.50 doesn't have access"
+    ssh root@10.10.10.50 mount -t nfs $nfs_export /mnt
+
+    echo "Permitting 10.10.10.50 to access the share:"
+    openstack share access create nfs-share ip 10.10.10.50
+    sleep $TIME
+
+    echo "Re-attempting mount, 10.10.10.50 now has access"
+    ssh root@10.10.10.50 mount -t nfs $nfs_export /mnt
+    ssh root@10.10.10.50 ls -l /mnt
+
+    sleep $TIME
+
+    echo "Mounting the CephFS share with native CEPHFS"
+    ssh root@10.10.10.50 umount /mnt
+    ssh root@10.10.10.50 mount -t cephfs $cephfs_export /mnt -o name=alice,secret=$cephx_secret
+
+    ssh root@10.10.10.50 touch /mnt/test5 /mnt/test6 /mnt/test7
+    ssh root@10.10.10.50 ls -l /mnt
+
 }
 
 test_manila
