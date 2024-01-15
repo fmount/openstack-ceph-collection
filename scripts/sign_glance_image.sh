493d6ee0bb382fb413246cd21be21bed51ed660d
Francesco Pantano <fpantano@redhat.com>
2024-01-15 12:20:26 +0100

    Image signature verification glance
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

---
+++ scripts/sign_glance_image.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# This is based on: https://docs.openstack.org/glance/latest/user/signature.html
+
+openssl genrsa -out private_key.pem 1024
+openssl rsa -pubout -in private_key.pem -out public_key.pem
+openssl req -new -key private_key.pem -out cert_request.csr
+openssl x509 -req -days 14 -in cert_request.csr -signkey private_key.pem -out new_cert.crt
+
+# create the secret
+openstack secret store --name test --algorithm RSA --expiration 2026-06-29 --secret-type certificate --payload-content-type "application/octet-stream" --payload-content-encoding base64 --payload "$(base64 new_cert.crt)"
+
+function build_image_signature {
+    echo This is a dodgy image > myimage
+    openssl dgst -sha256 -sign private_key.pem -sigopt rsa_padding_mode:pss -out myimage.signature myimage
+    base64 -w 0 myimage.signature > myimage.signature.b64
+}
+
+
+function create_signed_image {
+    local image_signature="$1"
+    local cert_uuid="$2"
+    glance --os-auth-url https://keystone-public-openstack.apps-crc.testing \
+           --os-project-name admin --os-username admin --os-password 12345678 \
+           --os-user-domain-name default --os-project-domain-name default \
+          image-create --name mySignedImage --container-format bare --disk-format qcow2 \
+          --property img_signature="$image_signature" --property img_signature_certificate_uuid="$cert_uuid" \
+          --property img_signature_hash_method='SHA-256' --property img_signature_key_type='RSA-PSS' < myimage
+}
+
+build_image_signature
+cert_uuid=$(openstack secret list -c Secret\ href -f value | cut -d '/' -f 6)
+image_signature=$(cat myimage.signature.b64)
+create_signed_image "$image_signature" "$cert_uuid"
