7addd2936ba9a1fefd5259a33051ba0bb22cb0ed
Francesco Pantano <fpantano@redhat.com>
2024-01-16 22:00:20 +0100

    Update glance image sign script
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- scripts/sign_glance_image.sh
+++ scripts/sign_glance_image.sh
@@ -2,7 +2,7 @@
 #
 # This is based on: https://docs.openstack.org/glance/latest/user/signature.html
 
-openssl genrsa -out private_key.pem 1024
+openssl genrsa -out private_key.pem 2048
 openssl rsa -pubout -in private_key.pem -out public_key.pem
 openssl req -new -key private_key.pem -out cert_request.csr
 openssl x509 -req -days 14 -in cert_request.csr -signkey private_key.pem -out new_cert.crt
@@ -12,7 +12,7 @@ openstack secret store --name test --algorithm RSA --expiration 2026-06-29 --sec
 
 function build_image_signature {
     echo This is a dodgy image > myimage
-    openssl dgst -sha256 -sign private_key.pem -sigopt rsa_padding_mode:pss -out myimage.signature myimage
+    openssl dgst -sha512 -sign private_key.pem -sigopt rsa_padding_mode:pss -out myimage.signature myimage
     base64 -w 0 myimage.signature > myimage.signature.b64
 }
 
@@ -25,7 +25,7 @@ function create_signed_image {
            --os-user-domain-name default --os-project-domain-name default \
           image-create --name mySignedImage --container-format bare --disk-format qcow2 \
           --property img_signature="$image_signature" --property img_signature_certificate_uuid="$cert_uuid" \
-          --property img_signature_hash_method='SHA-256' --property img_signature_key_type='RSA-PSS' < myimage
+          --property img_signature_hash_method='SHA-512' --property img_signature_key_type='RSA-PSS' < myimage
 }
 
 build_image_signature
