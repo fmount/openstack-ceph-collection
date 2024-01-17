e46c872ef80fdfb41091948cd9d072f28eec4073
Francesco Pantano <fpantano@redhat.com>
2024-01-17 13:21:11 +0100

    Improve sign_image script
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- scripts/sign_glance_image.sh
+++ scripts/sign_glance_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # This is based on: https://docs.openstack.org/glance/latest/user/signature.html
+# and it must be executed from the openstackClient POD
 
 openssl genrsa -out private_key.pem 1024
 openssl rsa -pubout -in private_key.pem -out public_key.pem
@@ -16,18 +17,20 @@ function build_image_signature {
     base64 -w 0 myimage.signature > myimage.signature.b64
 }
 
-
 function create_signed_image {
     local image_signature="$1"
     local cert_uuid="$2"
-    glance --os-auth-url https://keystone-public-openstack.apps-crc.testing \
-           --os-project-name admin --os-username admin --os-password 12345678 \
+    local admin_pwd="$3"
+    local keystone=$(cat $HOME/.config/openstack/clouds.yaml | awk '/auth_url/ {print $2}')
+    glance --os-auth-url $keystone \
+           --os-project-name admin --os-username admin --os-password "$admin_pwd" \
            --os-user-domain-name default --os-project-domain-name default \
           image-create --name mySignedImage --container-format bare --disk-format qcow2 \
           --property img_signature="$image_signature" --property img_signature_certificate_uuid="$cert_uuid" \
           --property img_signature_hash_method='SHA-512' --property img_signature_key_type='RSA-PSS' < myimage
 }
 
+admin_pwd=${1:-12345678}
 build_image_signature
 image_signature=$(cat myimage.signature.b64)
-create_signed_image "$image_signature" "$cert_uuid"
+create_signed_image "$image_signature" "$cert_uuid" "$admin_pwd"
